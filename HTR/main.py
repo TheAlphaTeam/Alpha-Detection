bd09d3494f3b5f36e620cc49ac66ed120f61e453
doaa-1996 <doaaobeidat1996@gmail.com>
2021-07-04 19:08:01 +0300

    detectingchar

--- HTR/main.py
+++ HTR/main.py
@@ -6,3 +6,17 @@ import time
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
+###############################################
+########### Detecting Characters ##############
+##############################################
+hImg, wImg,_ = img.shape
+boxes = pytesseract.image_to_boxes(img)
+for b in boxes.splitlines():
+    print(b)
+    b = b.split(' ')
+    print(b)
+    x, y, w, h = int(b[1]), int(b[2]), int(b[3]), int(b[4])
+    cv2.rectangle(img, (x,hImg- y), (w,hImg- h), (50, 50, 255), 1)
+    cv2.putText(img,b[0],(x,hImg- y+25),cv2.FONT_HERSHEY_SIMPLEX,1,(50,50,255),2)
+cv2.imshow('img', img)
+cv2.waitKey(0)

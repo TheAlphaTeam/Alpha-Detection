6ba28818851d79a538ed79001a1aac082d0300ef
Basel Mousa Atalla <baselatalla10@gmail.com>
2021-07-04 22:35:07 +0300

    add speak function which will pronounce the text of the image

--- HTR/main.py
+++ HTR/main.py
@@ -3,6 +3,7 @@ import re
 import pytesseract
 import numpy as np
 from PIL import ImageGrab
+import pyttsx3
 import time
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
@@ -13,7 +14,7 @@ pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesse
 def detecting_words(path):
     img = cv2.imread(path)
     # img = cv2.medianBlur(img, 5)
-    hImg, wImg, _ = img.shape
+    # hImg, wImg, _ = img.shape
     # img = cv2.resize(img, (int(wImg/1.3), int(hImg/1.3)))
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     boxes = pytesseract.image_to_data(img)
@@ -25,24 +26,43 @@ def detecting_words(path):
                 x, y, w, h = int(b[6]), int(b[7]), int(b[8]), int(b[9])
                 cv2.putText(img, b[11], (x, y - 4), cv2.FONT_HERSHEY_SIMPLEX, .7, (50, 50, 255), 2)
                 cv2.rectangle(img, (x, y), (x + w, y + h), (50, 50, 255), 2)
-
+    txt = print_whole_text(img)
     cv2.imshow('Detected words', img)
     cv2.waitKey(0)
+    # print(txt)
+    speak(txt)
+    return img, txt
 
 
 def print_whole_text(img):
     custom_config = r'-l eng+por+grc+tha+kor+spa+jpn+ara+rus --psm 6'
-    print(pytesseract.image_to_string(img, config=custom_config))
+    return pytesseract.image_to_string(img, config=custom_config)
 
 
 def print_only_digits(img):
     custom_config = r'--oem 3 --psm 6 outputbase digits'
-    print(pytesseract.image_to_string(img, config=custom_config))
+    return pytesseract.image_to_string(img, config=custom_config)
 
 
 def print_only_char(img):
     custom_config = r'-c tessedit_char_blacklist=0123456789 --psm 6'
-    print(pytesseract.image_to_string(img, config=custom_config))
+    return pytesseract.image_to_string(img, config=custom_config)
 
 
-detecting_words('data/shreek4.png')
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', 'voices[0].id')
+newVoiceRate = 145
+engine.setProperty('rate', newVoiceRate)
+
+
+def speak(txt):
+    """
+    this function which
+    converts text to speech
+    """
+    engine.say(txt)
+    engine.runAndWait()
+
+
+detecting_words('data/hitchhikers-rotated.png')

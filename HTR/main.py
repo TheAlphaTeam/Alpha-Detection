ff978e742958d1aac091dbbb9af01dcc8259c875
Basel Mousa Atalla <baselatalla10@gmail.com>
2021-07-04 17:48:27 +0300

    initait project

---
+++ HTR/main.py
@@ -0,0 +1,8 @@
+import cv2
+import pytesseract
+import numpy as np
+from PIL import ImageGrab
+import time
+
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+

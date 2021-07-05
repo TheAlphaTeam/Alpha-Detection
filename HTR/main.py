931640499bc3c2e52fd51f01736f9db291739a34
Sewar-web <s.maqableh@yahoo.com>
2021-07-05 12:34:52 +0300

    detect-only-digit

--- HTR/main.py
+++ HTR/main.py
@@ -8,25 +8,26 @@ pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesse
 
 ##### Detecting ONLY Digits  ######
 
-def only_digit(path):
+def detect_only_digit(path):
 
      img = cv2.imread(path)
      img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+     print(pytesseract.image_to_string(img))
      hImg, wImg,_ = img.shape
      conf = r'--oem 3 --psm 6 outputbase digits'
      boxes = pytesseract.image_to_boxes(img,config=conf)
      for b in boxes.splitlines():
-          print(b)
+          # print(b)
           b = b.split(' ')
-          print(b)
+          # print(b)
           x, y, w, h = int(b[1]), int(b[2]), int(b[3]), int(b[4])
-          cv2.rectangle(img, (x,hImg- y), (w,hImg- h), (50, 50, 255), 2)
+          cv2.rectangle(img, (x,hImg-y), (w,hImg-h), (50, 50, 255), 2)
           cv2.putText(img,b[0],(x,hImg- y+25),cv2.FONT_HERSHEY_SIMPLEX,1,(50,50,255),2)
      cv2.imshow('img', img)
      cv2.waitKey(0)
      return img
 
-only_digit('data/sewar1.png')
+detect_only_digit('data/sewar1.png')
 
 
 
